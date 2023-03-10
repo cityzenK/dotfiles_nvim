2935ec8535526674d8ea3e7f477879dd8344ab53
cityzenK <eliezergoh@gmail.com>
2023-03-09 22:33:06 -0600

    Add Picker git for branches

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,15 @@
+require('telescope').setup{
+    defaults = {
+        file_ignore_patterns = {"/node%_modules/.*", ".angular%/.*"}
+    }
+}
 local builtin = require('telescope.builtin')
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
-
+--Search Braches in Git repository
+vim.keymap.set('n','<leader>gb', function()
+    builtin.git_branches{}
+end)
