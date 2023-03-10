4ed2ca6c688c859730b39ffc966224a9130e1951
cityzenK <eliezergoh@gmail.com>
2023-03-09 22:32:08 -0600

    Adding a size function

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,3 +1,13 @@
+require("toggleterm").setup{
+    size =  function(term)
+        if term.direction == "horizontal" then
+            return 15
+        elseif term.direction == "vertical" then
+            return vim.o.columns * 0.4
+        end
+    end
+}
+
 function _G.set_terminal_keymaps()
   local opts = {buffer = 0}
   vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
